1ca976b568c2d3f0ce77d814ea49b235635c782b
cgzgithub <1944746232@qq.com>
2020-10-26 13:26:10 +0800

    更新

--- src/view/article/ArticleList.jsx
+++ src/view/article/ArticleList.jsx
@@ -1,22 +1,73 @@
 import React, {Fragment} from 'react';
 import apis from '@/api/apis';
+import {Table,Tag,Space} from 'antd'
+
+
 
 export default class ArticleList extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data:[]
+        }
+    }
+    goDetail(v){
+        console.log(v)
+    }
     componentDidMount(){
         console.log('apis>>>>>>>>>>>',apis)
         apis.getPublicArticleList().then(res => {
             console.log(res)
+            this.setState({
+                data:res.data
+            })
         })
     }
     render() {
+        const columns = [
+            {
+                title:'名称',
+                dataIndex:'articleTitle',
+                key:'articleTitle',
+                render: text => <a onClick={this.goDetail.bind(this,index)}>{text}</a>
+            },
+            {
+                title:'描述',
+                dataIndex:'articleDesc',
+                key:'articleDesc'
+            },
+            {
+                title:'创建时间',
+                dataIndex:'articleCdate',
+                key:'articleCdate',
+            },
+            {
+                title:'更新时间',
+                dataIndex:'articleUdate',
+                key:'articleUdate',
+            },
+            {
+                title:'操作',
+                dataIndex:'Action',
+                key:'action',
+                render: (text,record) => (
+                    <Space>
+                        <a>编辑</a>
+                        <a>删除</a>
+                    </Space>
+                )
+            }
+        ]
+        const {data} = this.state
         return(
             <Fragment>
                 <div>
                 文章列表页面
                 </div>
-                <div>
-                    
-                </div>
+                <Table
+                    columns={columns}
+                    dataSource={data}
+                ></Table>
             </Fragment>
         )
     }
